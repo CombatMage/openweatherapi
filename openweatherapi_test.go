87220cf6c94a688bfaa13a85b921bfbeef2ada43
Eric Neidhardt <eric.neidhardt@sevenval.com>
2017-07-12 10:28:44 +0200

    wip

--- openweatherapi_test.go
+++ openweatherapi_test.go
@@ -1,9 +1,23 @@
 package openweatherapi
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
+
+const apiKeyFile = "openweather.key"
+const cityBerlin = "Berlin,de"
+
+func readAPIKey() string {
+	key, err := ioutil.ReadFile(apiKeyFile)
+	if err != nil {
+		panic("cannot run test, you must provide openweathermap api key")
+	}
+	return string(key)
+}
 
 func TestDownloadWeatherData(t *testing.T) {
-	resp, err := DownloadWeatherData()
+	resp, err := DownloadWeatherData(readAPIKey(), cityBerlin)
 	if err != nil {
 		t.Error("error while retrieving data: " + err.Error())
 	} else if len(resp) == 0 {
