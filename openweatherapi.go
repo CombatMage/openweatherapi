fdccc9bf1440b166df30f4f968fac7fc8917d567
Eric Neidhardt <eric.neidhardt@sevenval.com>
2017-07-12 10:12:04 +0200

    removed key

--- openweatherapi.go
+++ openweatherapi.go
@@ -6,22 +6,19 @@ import (
 	"net/http"
 )
 
-const apiKey = "a50b478e72ffeeaf8850a5b72bb68865"
-const cityBerlin = "Berlin,de"
-
 // ForecastURL returns a string, used
 // url to query openweathermap.
-func ForecastURL() string {
+func ForecastURL(apiKey string, location string) string {
 	url := fmt.Sprintf(
 		"http://api.openweathermap.org/data/2.5/forecast/daily"+
-			"?q=%s&appid=%s&units=metric", cityBerlin, apiKey)
+			"?q=%s&appid=%s&units=metric", location, apiKey)
 	return url
 }
 
 // DownloadWeatherData downloads forecast data from
 // openweathermap and return them as string
-func DownloadWeatherData() (json string, err error) {
-	resp, err := http.Get(ForecastURL())
+func DownloadWeatherData(apiKey string, location string) (json string, err error) {
+	resp, err := http.Get(ForecastURL(apiKey, location))
 	if err != nil {
 		return "", err
 	}
